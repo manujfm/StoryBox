55d1b94e17bd5633adc6256ca6c7ae87d4e5919f
Manuel Marcano <manuel.marcano@vivacetek.com>
2017-09-29 22:21:56 -0400

    Responsive Image

--- public/js/java.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($) {
-	$("p").click(function() {
-	console.log("Hola")
-});
-});
-
---
+++ public/js/responsive-image.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+
+if(window.screen.width<500){
+	$(".image-title").css("width","100%");
+	$(".logo").css("width","323px");
+	$(".logo").css("height","200px");
+	$(".logo").css("margin-left","0");
+}
+
+}); //End Script
