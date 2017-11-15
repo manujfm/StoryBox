a3da31b8db936573cd9789cb9326b9c009a4ac5e
Manuel Marcano <manuel.marcano@vivacetek.com>
2017-11-15 11:19:26 -0300

    end

--- controlers/controler-box.js
+++ controlers/controler-box.js
@@ -201,7 +201,7 @@ var BoxModel=require('../model/sql-models'),
                 BoxModel.insertData(req.body,'doc/images/'+file_newname,null,(err,row)=>{
                   if (err) {
                     console.log(err)
-                    res.render("no existe cube")
+                    res.end("no existe cube")
                   }else {
                      res.redirect("/content-manipulation")
                      res.end();
