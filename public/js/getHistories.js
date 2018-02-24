28daf740f40888f92e9096e20ec6272c3b294f60
Manuel Marcano <manuel.marcano@vivacetek.com>
2018-02-24 01:40:15 -0300

    Responsive

--- public/js/getHistories.js
+++ public/js/getHistories.js
@@ -15,25 +15,27 @@
 
         box.date=dateTransform(box.date)
          content+= `
-	       <div class="col-lg-3 col-xm-12">
+	       <div class="col-lg-3 col-lg-offset-0 col-xs-9 col-xs-offset-2">
+           <div class="col-lg-12">
+    	          <div class="info">
+    	           <a href="Stories/id=${box.id}">
+    	           <h3 id="headCont">${box.title}</h3>
+    	           </a>
+    	           <p id="parrafo">${box.summary}</p>
+    	         </div>
 
-	         <div style="margin-left:25px" class="info">
-	           <a href="Stories/id=${box.id}">
-	           <h3 id="cabeza">${box.title}</h3>
-	           </a>
-	           <p id="parrafo">${box.summary}</p>
-	         </div>
+    	         <img src="${box.url}" alt="${box.title}" height="240px" width="240px" style="border-radius: 100%;"/>
+           </div>
 
-	         <img src="${box.url}" alt="${box.title}" height="240px" width="240px" style="border-radius: 100%; margin-left:25px"/>
-
-	         <div class="title-post">
-	           <a href="Stories/id=${box.id}">
-	            <h2 align="center">${box.title}</h2>
-	           </a>
-	           <p align="center">${box.date}</p>
-	         </div>
-	     
-	       </div>`
+           <div class="col-lg-8  col-lg-offset-2 col-xs-8 col-xs-offset-2">
+            <div class="title-post">
+              <a href="Stories/id=${box.id}">
+                <h2 align="center">${box.title}</h2>
+              </a>
+              <p align="center">${box.date}</p>
+            </div>
+           </div>
+         </div>`
 	       if(count%8==0||count==tam){
               pages.push(content)
               content=""
